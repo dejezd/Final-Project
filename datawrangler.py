3f981b076be05a22ee50b4f59ac58f53b30d15da
dejezd <jeremy@jezd.net>
2019-12-05 13:55:49 -0500

    Should have everything I need to run a test

--- datawrangler.py
+++ datawrangler.py
@@ -19,15 +19,24 @@ def main():
     files_path = 'Data/'
     read_files = glob.glob(os.path.join(files_path, "*.txt"))
     i = 0
+    labels =[]
     # Initialize empty arrays to take on samples
-
     for file in read_files:
-        emg_data = pd.read_csv(file, header=0, sep='\t')
+        emg_data = pd.read_csv(file, header=0, sep='\t')  # Read in an individual file
         print(emg_data.head())
-        pemg = process(emg_data)
-        print(pemg)
-        sample_gen(pemg, i)
+        pemg = process(emg_data)  # Label each class change as a separate gesture
+        #print(pemg)
+        label = sample_gen(pemg, i)  # Generate output files and a vector of classes
+        # Create a super list of labels for use in the RNN
+        if i == 0:
+            labels = label
+        else:
+            labels = np.append(labels, label)
         i = i + 1
+    # Orient the Labels the way that I want it to
+    labels = np.transpose(labels)
+    np.savetxt("PData/emg_labels.csv", labels, delimiter=',')  # Save to a csv file
+
     #emg1 = pd.read_csv("data/1_raw_data_13-12_22.03.16.txt", header=0, sep='\t')
     #print(emg1.head())
     #emg1 = emg1.to_numpy()
@@ -42,7 +51,7 @@ def process(data):
     """
     tdata = data
     table = np.c_[tdata, (np.zeros(np.size(tdata, 0)))]
-    print(table)
+    #print(table)
     j = 0
     for i in range(0, np.size(table, 0)):
         if i == 0:
@@ -60,10 +69,12 @@ def sample_gen(data, fnum):
     :param data: The data set processed using the process function above, this will allow us to parse the dataset to
                  what it needs to be to export and be used in the model.
     :param fnum: This will define what the origin file of each sample, ordered from top to bottom from the Data Folder
-    :return: This function does not have a return, as it saves files to a local directory instead.
+    :return: This function returns a 1x11 array of the class label for each gesture processed.
     """
     maxindex = np.size(data, 0) - 1
     k = 0  # Used in File Naming, to enumerate the gestures of a particular file
+    m = 0
+    label = np.zeros(1)
     for i in range(0, int(data[maxindex, 10])):
         table = []
         count = 0
@@ -72,17 +83,24 @@ def sample_gen(data, fnum):
                 if data[j, 9] != 0 and data[j, 10] == i:
                     table = np.r_[table, data[j, :]]
                     count = count + 1
+                    if m == 0:
+                        label[m] = data[j, 9]
+                    else:
+                        label = np.append(label, data[j, 9])
             elif data[j, 9] != 0 and data[j, 10] == data[j-1, 10]:
                 table = np.c_[table, data[j, :]]
             else:
+                m = m+1
                 break
         if np.size(table, 0) != 0:
             print("Creating file for Sample ", fnum, "Number ", k)
-            print(table)
+            #print(table)
+            table = table[1:9, :]
             table = np.transpose(table)
             np.savetxt("PData/emg_sample_%d_%d.csv" % (fnum, k), table, delimiter=',')
             k = k+1
-    return
+    print(label)
+    return label
 
 
 if __name__ == '__main__':
--- rnn.py
+++ rnn.py
@@ -7,24 +7,55 @@
 ##
 
 from __future__ import absolute_import, division, print_function, unicode_literals
-import tensorflow_datasets as tfds
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.model_selection import train_test_split
 import pandas as pd
+import glob
 import os
 
-# Import the Dataset from PData, and process it for use in the model again.
-# Need to create lists of entries from each channel of data, I think.
+# Import a list of file names to split up to form the dataset
+files_path = 'PData/'
+dataset = [None]*872
+fnames = glob.glob(os.path.join(files_path, 'emg_sample_*.csv'))
+i = 0
+# Create a data structures to separate instances into numpy arrays to put into the model
+for names in fnames:
+    dataset[i] = np.loadtxt(names, delimiter=',')
+# Load in labels vector
+flabels = np.loadtxt('PData/emg_labels.csv', delimiter=',')
+##
+# Create a loop with sequentially named datasets to set up testing and traning filenames
+# Need to parse it down using the data.shape file, and set the data to data[:, 1:9]
+# Set Labels to add data[0, 10], to a list for use as labels.
+#print('Creating Test and Training Data sets')
+train, test, train_l, test_l = train_test_split(dataset, flabels, test_size=0.25, random_state=1)
+#print(np.size(train))
+#print(np.size(test))
+##
 
-# Create the model
+# Start to set up the data for the sets
+# Need to find a way to run the data for the model by reading the csv, and identifying the labels from it.
+# Resource: https://datascience.stackexchange.com/questions/51249/training-keras-model-with-multiple-csv-files
+# LSTM Start
 model = tf.keras.Sequential([
-    #Needs some sort of embedding layer here, look into this more
-    tf.keras.layers.Bidirectional(tf.kerasa.layers.LSTM(64)),
-    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(16)), # Will play around with more hyperparameters here.
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(16, activation='relu'),
     tf.keras.layers.Dense(7, activation='softmax')
 ])
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer=tf.keras.optimizers.Adam(1e-4),
-              metrics=['accuracy'])
-# Use the model on each of the different channels here (Maybe initialize it a bunch of times?)
+
+model.compile(optimizer=tf.keras.optimizers.Adam(1e-4),
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+history = model.fit(train, epochs=10,
+                    validation_data=test,
+                    validation_steps=30)
+test_loss, test_acc = model.evaluate(test)
+print('Test Loss: {}'.format(test_loss))
+print('Test Accuracy: {}'.format(test_acc))
+
+# Insert Plotting functions here
+
+
