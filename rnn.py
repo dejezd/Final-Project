b8c6ab420497d83d6e80a1c1f08c066f94f0b7c4
Dejezd <jeremy@jezd.net>
2019-12-13 16:38:14 -0500

    Updated data processing, still non-functional

--- rnn.py
+++ rnn.py
@@ -17,16 +17,23 @@ import os
 
 
 def main():
-    list_emg = tf.data.Dataset.list_files('PData/emg_sample_*.csv')
-    train_size = int(0.75*872)
-    list_emg = list_emg.shuffle(872)  # Shuffles data taking the whole dataset into account.
-    train = list_emg.take(train_size)
-    test = list_emg.skip(train_size)
-    for f in train.take(1):
-        print(f.numpy())
+    #list_emg = tf.data.Dataset.list_files('PData/emg_sample_*.csv')
+    #train_size = int(0.75*872)
+    #list_emg = list_emg.shuffle(872)  # Shuffles data taking the whole dataset into account.
+    #train = list_emg.take(train_size)
+    #test = list_emg.skip(train_size)
+    #for f in train.take(1):
+     #   print(f.numpy())
         # print(tf.io.read_file(f))
-    labeled_tds = train.map(process_data)
-    labeled_test = test.map(process_data)
+    #labeled_tds = train.map(process_data(train, True))
+    #labeled_test = test.map(process_data(test, False))
+    files_name = 'PData/'
+    fnames = glob.glob(os.path.join(files_name, 'emg_sample_*.csv'))
+    train_set, test_set = train_test_split(fnames, test_size=0.25, random_state=1)
+    train = tf.data.Dataset.from_tensor_slices(train_set)
+    test = tf.data.Dataset.from_tensor_slices(test_set)
+    labeled_tds = train.map(process_data(train_set, True))
+    labeled_test = test.map(process_data(test_set, False))
     model = gen_model()
     model.compile(optimizer=tf.keras.optimizers.Adam(1e-4),
                   loss='sparse_categorical_crossentropy',
@@ -53,7 +60,7 @@ def gen_model():
     """
     print('Initializing Model...')
     model = tf.keras.Sequential([
-        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(16)), # Will play around with more hyperparameters here.
+        tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(16)),  # Will play around with more hyperparameters here.
         tf.keras.layers.Dense(16, activation='relu'),
         tf.keras.layers.Dense(8, activation='relu'),
         tf.keras.layers.Dense(7, activation='softmax')
@@ -61,29 +68,30 @@ def gen_model():
     return model
 
 
-def process_data(file_path, test):
+def process_data(file_path, train):
     """
     This processes the data to enter a tensorflow dataset. It sets the label to the 9th column,
     which indicates the gesture. The rest is put into the data section as a numpy array to be fed to the network.
     :param file_path: The files with which the
-    :param: test: Boolean, shows if data set size is the test or training sets.
+    :param train: Boolean, shows if data set size is the test or training sets.
     :return:
     """
-    if test == True:
+    if train == True:
         size = 654
     else:
         size = 218
     # This might need a for loop to run through each file and return it to the map file?
-    data_all = np.zeros(size)
+    data_all = [None]*size
     label_all = np.zeros(size)
     i = 0
     for f in file_path:
-        file = np.loadtxt(f.numpy(), delimiter=',')
+        file = np.loadtxt(f, delimiter=',')
         data = file[0:8, :]  # Should input all 8 columns of data needed
         data_all[i] = data
         label = file[1, 8]
-        label_all[i]= label
+        label_all[i] = label
         i = i+1
+    data_all = np.asarray(data_all)
     return data_all, label_all
 
 
